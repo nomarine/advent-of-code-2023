319bd3d535dad35d5e59188979bce904c0621b7f
nomarine <rafaelfijos@gmail.com>
2023-12-06 11:40:22 -0300

    Get cube minimum quantities

---
+++ 02-Cube Conundrum/Part2.php
@@ -0,0 +1,62 @@
+<?php
+    /* $document = file_get_contents('games.txt'); */
+    $document = file_get_contents('poc.txt');
+    $dataset = explode("\n", $document);
+    $cube_colors = ['red', 'blue', 'green'];
+    $result = 0;
+    $games = setGames($dataset, $cube_colors);
+    
+    $cube_limits = setCubeLimits($cube_colors, $games);
+    foreach($cube_limits as $game_number=>$game){
+        echo 'Game '.$game_number." could have been played with a minimum of:\n";
+        foreach($game as $color => $min){
+            echo $min.' '.$color." cubes \n";
+        }
+        echo "\n";
+    }
+
+    function setGames($dataset, $cube_colors){
+        foreach($dataset as $line){
+            $pattern = '/game.(\d+)/i';
+            preg_match($pattern, $line, $matches);
+            
+            $index = $matches[1];
+            $games[$index] = [];
+            
+            setCubes($cube_colors, $games[$index], $line);
+        }
+        return $games;
+    }
+
+    function setCubes($cube_colors, &$game, $line){
+        foreach($cube_colors as $color){            
+            $pattern = '/(\d+).'.$color.'/i';
+            preg_match_all($pattern, $line, $matches);
+            
+            if(!empty($matches[0])){
+                foreach($matches[1] as $quantity){
+                    $game[$color][] = $quantity;
+                }
+            }
+        }
+    }
+
+    function setCubeLimits($cube_colors, $games){
+        foreach($games as $game_number => $game){
+            $cube_limits[$game_number] = [];
+            foreach($cube_colors as $color){
+                $cube_limits[$game_number][$color] = max($game[$color]);
+            }
+        }
+        return $cube_limits;
+    }
+
+    function getResult($game_numbers){
+        $result = 0;
+        foreach($game_numbers as $number){
+            $result += $number;
+        }
+        return $result;
+    }
+    
+?>
---
+++ 02-Cube Conundrum/game-settings.php
@@ -0,0 +1,7 @@
+<?php
+    $cube_quantity = [
+        'red' => 12,
+        'blue' => 14,
+        'green' => 13
+    ];
+?>
