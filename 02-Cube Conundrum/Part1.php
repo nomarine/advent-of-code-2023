d53c155e2ebd08902cc41de42b18a7a9347e9853
nomarine <rafaelfijos@gmail.com>
2023-12-06 10:28:53 -0300

    Store cube quantity in associative array

--- 02-Cube Conundrum/Part1.php
+++ 02-Cube Conundrum/Part1.php
@@ -1,38 +1,34 @@
 <?php
     $document = file_get_contents('games.txt');
-    $lines = explode("\n", $document);
+    $dataset = explode("\n", $document);
     $cube_colors = ['red', 'blue', 'green'];
     $result = 0;
-    setGames($lines);
+    $games = setGames($dataset, $cube_colors);
+    print_r($games);
 
-    function setGames($dataset){
-        $games = [[]];
+    function setGames($dataset, $cube_colors){
         foreach($dataset as $line){
             $pattern = '/game.(\d+)/i';
             preg_match($pattern, $line, $matches);
-            /* print_r($matches); */
+            
             $index = $matches[1];
-
-            /* print_r($games); */
-            setCubes($colors, $game[$index], $line);
+            $games[$index] = [];
+            
+            setCubes($cube_colors, $games[$index], $line);
         }
+        return $games;
     }
 
-    function setCubes($colors, $game, $line){
-        foreach($colors as $color){
-            $game = [
-                $color => []
-            ];
-        }
-        
-        $pattern = '/(\d+).green/i';
-        preg_match_all($pattern, $line, $matches);
-        if(!empty($matches[0])){
-            foreach($matches[1] as $quantity){
-                /* print_r($quantity."\n"); */
-                $game['green'][] = $quantity;
+    function setCubes($cube_colors, &$game, $line){
+        foreach($cube_colors as $color){            
+            $pattern = '/(\d+).'.$color.'/i';
+            preg_match_all($pattern, $line, $matches);
+            
+            if(!empty($matches[0])){
+                foreach($matches[1] as $quantity){
+                    $game[$color][] = $quantity;
+                }
             }
-            print_r($game['green']);
         }
     }
     
